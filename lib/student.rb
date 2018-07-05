76bfb4f4d15223bde58b3f0536c80796b16e9c5a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-05 12:28:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT x
+      LIMIT x desc
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
