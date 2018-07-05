c65511d16119ed16e78bf07c60ae28bd6239533d
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-05 12:08:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE name = ?
-      LIMIT 1
+      WHERE grade = 9
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
