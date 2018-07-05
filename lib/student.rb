1633a55674f1ff9d1086b65aae9806ccd4e110ec
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-05 12:29:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
       ORDER BY students.id
       LIMIT ?
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql,number).map do |row|
       self.new_from_db(row)
     end
   end
